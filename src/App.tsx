08bfddb2d261b43af446bf144cc3e1c13b5aa08d
regchiu <regx1385@gmail.com>
2025-04-20 20:44:58 +0800

    refactor: move PostPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Layout from './components/Layout/Layout'
 import SubredditList from '@/features/subreddits/SubredditList/SubredditList'
 import SearchTermInput from '@/features/subredditPosts/SearchTermInput/SearchTermInput'
-import PostPage from './features/subredditPosts/PostPage/PostPage'
+import PostPage from '@/pages/PostPage'
 
 function App() {
   return (
